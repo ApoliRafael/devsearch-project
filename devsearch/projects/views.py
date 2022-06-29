b82284e067d1c151acdc3ec736d63b29924ccdea
rafael.alves <rafael.alves@huia.haus>
2022-06-29 18:23:36 -0300

    Sessão 2 concluída - Views & URL's, Templates & Template Inheritance, Rendering Data to Templates

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id':'2',
+        'title':'Portfolio Website',
+        'description':'This was a project where I built out my portfolio'
+    },
+    {
+        'id':'3',
+        'title':'Social Network',
+        'description':'Awesome open source project I am still working on'
+    },
+]
+
+def projects(request):
+    page = "Projects"
+    number = 122
+    context = {
+        'page' : page,
+        'number' : number,
+        'projectsList' : projectsList,
+    }
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project' : projectObj})
